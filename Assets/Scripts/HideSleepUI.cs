150b4a7da5008a1ea51c5119bc2c80267b48d89d
therandommer <T7083588@tees.ac.uk>
2021-05-07 17:38:12 +0100

    Sleeping implemented
    Title

---
+++ Assets/Scripts/HideSleepUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideSleepUI : MonoBehaviour
+{
+    public GameObject objectToHide = null;
+    void Start()
+    {
+        if(objectToHide == null)
+		{
+            gameObject.SetActive(false); //deactivates this button if nothing to hide
+		}
+    }
+
+    public void ToggleObject(bool _newState)
+	{
+        objectToHide.gameObject.SetActive(_newState);
+	}
+}
